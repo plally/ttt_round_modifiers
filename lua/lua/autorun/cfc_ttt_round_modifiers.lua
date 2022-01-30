ab8f38695260085c660b33a0359f000bbd31425e
Pierce <pierce@vulpes.dev>
2022-01-30 11:26:05 -0500

    fix  file.Find call

---
+++ lua/lua/autorun/cfc_ttt_round_modifiers.lua
@@ -0,0 +1,31 @@
+RoundModifiers = {}
+RoundModifiers.modifiers = {}
+
+local files, directories =  file.Find("modifiers/*", "LUA")
+
+for _, file in ipairs(files or {}) do
+    MODIFIER = {}
+    include(file)
+    AddCSLuaFile(file)
+    local name = string.sub(file, 1, #file - 4)
+    if MODIFIER.name then 
+        name = MODIFIER.name 
+    end
+
+    RoundModifiers.modifiers[name] = MODIFIER
+end
+
+-- TODO support for serverside and clientside files
+for _, dir in ipairs(dir or {}) do
+    local sharedFile = dir .. "/" .. "sh_init.lua" 
+
+    MODIFIER = {}
+    include(file)
+    AddCSLuaFile(file)
+    local name = dir
+    if MODIFIER.name then 
+        name = MODIFIER.name 
+    end
+
+    RoundModifiers.modifiers[name] = MODIFIER
+end
